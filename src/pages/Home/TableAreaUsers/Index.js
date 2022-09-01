2bae5a5f5e3f69db51c923d261456c77f33ba242
seu nome <michelcm13@gmail.com>
2022-09-01 14:22:15 -0300

    ----

--- src/pages/Home/TableAreaUsers/Index.js
+++ src/pages/Home/TableAreaUsers/Index.js
@@ -7,54 +7,54 @@ import IconSeeB from "../../../assets/actions/seeb.png";
 export const TableAreaUsers = () => {
   return (
     <C.Container>
-      <div class="container-table100">
-        <div class="wrap-table100">
-          <div class="table">
-            <div class="row header">
-              <div class="cell">Nome</div>
-              <div class="cell">E-mail</div>
-              <div class="cell">Predominancia</div>
-              <div class="cell">Data</div>
-              <div class="cell"></div>
-              <div class="cell"></div>
-              <div class="cell"></div>
+      <div className="container-table100">
+        <div className="wrap-table100">
+          <div className="table">
+            <div className="row header">
+              <div className="cell">Nome</div>
+              <div className="cell">E-mail</div>
+              <div className="cell">Predominancia</div>
+              <div className="cell">Data</div>
+              <div className="cell"></div>
+              <div className="cell"></div>
+              <div className="cell"></div>
             </div>
 
-            <div class="row">
-              <div class="cell" >
+            <div className="row">
+              <div className="cell" >
                 Michel Corrêa Martins
               </div>
-              <div class="cell" >
+              <div className="cell" >
                 michel-cm15@hotmail.com
               </div>
-              <div class="cell" >
+              <div className="cell" >
                 Condescendente
               </div>
-              <div class="cell">
+              <div className="cell">
                 29/08/2022
               </div>
-              <div class="cell " >
+              <div className="cell " >
                 <a href="#">
                   <img
-                    class=" icon iconSee"
+                    className=" icon iconSee"
                     src={IconSeeB}
                     alt="icon see"
                   />{" "}
                 </a>               
               </div>
-              <div class="cell iconsLista" >
+              <div className="cell iconsLista" >
                 <a href="#">
                   <img
-                    class=" icon iconSee"
+                    className=" icon iconSee"
                     src={IconDeleteB}
                     alt="icon see"
                   />{" "}
                 </a>               
               </div>
-              <div class="cell iconsLista" >
+              <div className="cell iconsLista" >
                 <a href="#">
                   <img
-                    class=" icon iconSee"
+                    className=" icon iconSee"
                     src={IconStarB}
                     alt="icon see"
                   />{" "}
